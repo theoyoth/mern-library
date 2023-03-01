7a42ab16b752693c48926782f8228231f3f4f3dd
theoyoth <theobantang1@gmail.com>
2023-03-01 20:52:27 +0700

    change order

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(cors({origin:["http://127.0.0.1:5173","https://booklibraryapp.vercel.app"]}));
 
 app.use(express.json());
-connectDB();
 app.use(route);
+connectDB();
 
 app.listen(3001,() => {
     console.log("server running on port 3001");
