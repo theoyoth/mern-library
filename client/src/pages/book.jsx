b467b51fe29aa5ea877a51a6a039f7ce0aceac52
theoyoth <theobantang1@gmail.com>
2023-03-01 21:34:50 +0700

    change search input in mobile

--- client/src/pages/book.jsx
+++ client/src/pages/book.jsx
@@ -53,14 +53,14 @@ const book = () => {
       <Title title='Book list' />
       <div className='flex justify-between items-center'>
         <Link to="/post" className='px-2 py-2 border-4 border-softblack transition-all duration-400 ease-in-out bg-softwhite hover:bg-blue-600 text-softblack hover:text-softwhite text-sm sm:text-[16px]'>add book</Link>
-        <div className='flex gap-2 items-center'>
+        <div className='flex gap-2 items-center border border-black'>
           <input required
               type="text"
               name="search"
               onChange={(e) => setSearch(e.target.value)}
               value={search}
               placeholder="search book"
-              className='block w-full shadow-sm text-black px-4 py-2 focus:outline-none bg-softwhite focus:border-softblack focus:ring-4 focus:ring-softblack text-sm sm:text-[16px]'
+              className='block w-[150px] sm:w-full shadow-sm text-black px-4 py-2 focus:outline-none bg-softwhite focus:border-softblack focus:ring-4 focus:ring-softblack text-sm sm:text-[16px]'
           />
         </div>
       </div>
