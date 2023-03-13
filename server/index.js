9d622d4d1efdb9fe2ede68b4c93e79e6790ecc7e
theoyoth <theobantang1@gmail.com>
2023-03-13 12:09:31 +0700

    install cookie parser

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import * as dotenv from 'dotenv'
 dotenv.config()
 
@@ -8,8 +9,8 @@ import connectDB from "./db.js";
 
 const app = express();
 
+app.use(cookieParser())
 app.use(cors({origin:['https://booklibraryapp.vercel.app','http://127.0.0.1:5173','https://mern-library-api.vercel.app','http://localhost:3000'],methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']}));
-
 app.use(express.json());
 app.use(route);
 connectDB();
