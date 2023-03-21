c4052ec7d187d0505331ae996ce350dbf3394201
theoyoth <theobantang1gmail.com>
2023-03-21 20:01:20 +0700

    add footer

--- client/src/Layout/Layoutpage.jsx
+++ client/src/Layout/Layoutpage.jsx
@@ -3,6 +3,8 @@ import Navbar from '../components/Navbar'
 
 import {useIsAuthenticated} from 'react-auth-kit';
 
+const date = new Date()
+
 function Layoutpage() {
   const isAuthenticated = useIsAuthenticated()
 
@@ -10,6 +12,7 @@ function Layoutpage() {
     <div className='h-full text-[#d2d2d2] md:px-[10rem] lg:px-[20rem] px-[2rem] py-2 overflow-x-hidden'>
       {isAuthenticated() && <Navbar />}
       <Outlet />
+      <p className='text-[10px] text-center absolute left-[50%] bottom-[15px] -translate-x-[50%]'>&copy;{date.getFullYear()} | Theoyoth | All rights reserved.</p>
     </div>
   )
 }
